afe013ee953bd3b6e11b05e5e8138976af8082f9
CarpetFloor <dbartness001@csbsju.edu>
2024-03-02 23:12:00 -0600

    add rule

---
+++ script.js
@@ -0,0 +1,37 @@
+function addRule(elem) {
+    let container = elem.parentNode.parentNode;
+
+    let ruleContainer = document.createElement("div");
+    ruleContainer.className = "ruleContainer";
+
+    let pipe = document.createElement("p");
+    pipe.innerHTML = "|&nbsp;&nbsp;&nbsp;";
+    
+    let input = document.createElement("input");
+    input.type = "text";
+    input.value = "...HERE";
+    
+    let addRemoveRule = document.createElement("div");
+    addRemoveRule.className = "addRemoveRule";
+    
+    let addButton = document.createElement("button");
+    addButton.innerText = "+";
+    addButton.addEventListener("click", function(){addRule(addButton)});
+    
+    let removeButton = document.createElement("button");
+    removeButton.innerText = "-";
+    removeButton.addEventListener("click", function(){removeRule(removeButton)});
+
+    addRemoveRule.appendChild(addButton);
+    addRemoveRule.appendChild(removeButton);
+    
+    ruleContainer.appendChild(addRemoveRule);
+    ruleContainer.appendChild(pipe);
+    ruleContainer.appendChild(input);
+
+    container.insertAdjacentElement("afterend", ruleContainer);
+}
+
+function removeRule(elem) {
+    console.log("remove");
+}
