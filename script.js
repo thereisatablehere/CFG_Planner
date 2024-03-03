598ba67f47e7e771328435fe27d6e1f4fee1e11b
CarpetFloor <dbartness001@csbsju.edu>
2024-03-03 00:11:30 -0600

    add statement

--- script.js
+++ script.js
@@ -76,3 +76,75 @@ function removeRule(elem) {
         parent.appendChild(ruleContainer);
     }
 }
+
+function addStatement(elem) {
+    let statement = elem.parentNode.parentNode;
+
+    let newStatement = document.createElement("div");
+    newStatement.className = "container";
+
+    let statementContainer = document.createElement("div");
+    statementContainer.className = "statementContainer";
+
+    let firstInput = document.createElement("input");
+    firstInput.type = "text";
+    firstInput.value = ".";
+
+    statementContainer.appendChild(firstInput);
+
+    let rightSide = document.createElement("div");
+    rightSide.className = "rightSide";
+
+    let ruleContainerTop = document.createElement("div");
+    ruleContainerTop.className = "ruleContainer";
+
+    let arrow = document.createElement("p");
+    arrow.innerHTML = "->";
+
+    let input = document.createElement("input");
+    input.type = "text";
+    input.value = ".";
+
+    ruleContainerTop.appendChild(arrow);
+    ruleContainerTop.appendChild(input);
+
+    rightSide.appendChild(ruleContainerTop);
+
+    let ruleContainerBottom = document.createElement("div");
+    ruleContainerBottom.className = "ruleContainer";
+
+    let firstAddRemoveRule = document.createElement("div");
+    firstAddRemoveRule.className = "firstAddRemoveRule";
+
+    let firstAddButton = document.createElement("button");
+    firstAddButton.innerText = "+";
+    firstAddButton.addEventListener("click", function(){addFirstRule(firstAddButton)});
+
+    firstAddRemoveRule.appendChild(firstAddButton);
+
+    ruleContainerBottom.appendChild(firstAddRemoveRule);
+
+    rightSide.appendChild(ruleContainerBottom);
+
+    statementContainer.appendChild(rightSide);
+
+    newStatement.appendChild(statementContainer);
+    
+    let addRemoveStatement = document.createElement("div");
+    addRemoveStatement.className = "addRemoveStatement";
+
+    let addButton = document.createElement("button");
+    addButton.innerText = "+";
+    addButton.addEventListener("click", function(){addStatement(addButton)});
+    
+    let removeButton = document.createElement("button");
+    removeButton.innerText = "-";
+    // removeButton.addEventListener("click", function(){removeRule(removeButton)});
+
+    addRemoveStatement.appendChild(addButton);
+    addRemoveStatement.appendChild(removeButton);
+
+    newStatement.appendChild(addRemoveStatement);
+
+    statement.insertAdjacentElement("afterend", newStatement);
+}
